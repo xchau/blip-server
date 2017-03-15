3a2de28df77900aab3fd3d67586d56a85bed0ba1
Minh Chau <xiu.chau@gmail.com>
2017-03-15 13:19:10 -0700

    Test 400 error message

--- server/app.js
+++ server/app.js
@@ -24,6 +24,7 @@ app.use((req, res) => {
 
 app.use((err, req, res, _next) => {
   if (err.output && err.output.statusCode) {
+    console.log('conditional: ', err);
     return res
       .status(err.output.statusCode)
       .set('Content-Type', 'text/plain')
