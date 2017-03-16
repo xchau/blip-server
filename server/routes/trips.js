b249dba34bb16aa5d173b0e2a1528cc2520b56c4
Minh Chau <xiu.chau@gmail.com>
2017-03-16 14:58:24 -0700

    Send user's name with trips res

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -9,7 +9,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
   knex('trips')
-    .select('trips.id, trips.destination, trips.subtitle, trips.votes, trips.updated_at, trips.cover_photo, users.name')
+    .select('trips.id', 'trips.user_id', 'trips.destination', 'trips.subtitle', 'trips.cover_photo', 'trips.votes', 'trips.updated_at', 'users.name')
     .orderBy('trips.created_at', 'DESC')
     .innerJoin('users', 'users.id', 'trips.user_id')
     .then((rows) => {
