be6e0ae1cd2f3d8fd0ad11d54f605bcfedb37fbf
Minh Chau <xiu.chau@gmail.com>
2017-03-19 15:24:14 -0700

    Change directions to notes in entries seed

--- seeds/3_entries.js
+++ seeds/3_entries.js
@@ -9,7 +9,7 @@ exports.seed = function(knex) {
           id: 1,
           trip_id: 1,
           entry_title: 'A Night in Shangri-La',
-          directions: 'Vinyl four dollar toast hammock, semiotics freegan craft beer keytar.',
+          notes: 'Vinyl four dollar toast hammock, semiotics freegan craft beer keytar.',
           created_at: new Date('2017-05-16 14:26:16 UTC'),
           updated_at: new Date('2017-05-16 14:26:16 UTC')
         },
@@ -17,7 +17,7 @@ exports.seed = function(knex) {
           id: 2,
           trip_id: 1,
           entry_title: 'Swayambhu Temple',
-          directions: 'Occupy snackwave raw denim listicle freegan edison bulb.',
+          notes: 'Occupy snackwave raw denim listicle freegan edison bulb.',
           created_at: new Date('2017-05-16 15:46:16 UTC'),
           updated_at: new Date('2017-05-16 15:46:16 UTC')
         },
@@ -25,7 +25,7 @@ exports.seed = function(knex) {
           id: 3,
           trip_id: 1,
           entry_title: 'Boudha Stupa',
-          directions: 'Mustache quinoa woke, kitsch normcore synth raw denim kinfolk.',
+          notes: 'Mustache quinoa woke, kitsch normcore synth raw denim kinfolk.',
           created_at: new Date('2017-05-16 17:14:16 UTC'),
           updated_at: new Date('2017-05-16 17:14:16 UTC')
         },
@@ -33,7 +33,7 @@ exports.seed = function(knex) {
           id: 4,
           trip_id: 1,
           entry_title: 'Everest Basecamp',
-          directions: 'Hexagon pinterest lo-fi pok pok vexillologist.',
+          notes: 'Hexagon pinterest lo-fi pok pok vexillologist.',
           created_at: new Date('2017-05-18 06:14:16 UTC'),
           updated_at: new Date('2017-05-18 06:14:16 UTC')
         },
@@ -41,7 +41,7 @@ exports.seed = function(knex) {
           id: 5,
           trip_id: 1,
           entry_title: 'Conquering Everest',
-          directions: 'Gentrify health goth locavore banjo tofu DIY woke.',
+          notes: 'Gentrify health goth locavore banjo tofu DIY woke.',
           created_at: new Date('2017-05-20 08:29:16 UTC'),
           updated_at: new Date('2017-05-20 08:29:16 UTC')
         },
