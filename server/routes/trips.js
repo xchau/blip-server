2a8c8d31f1025ac79d526e24c7c58ddea831b8e2
Minh Chau <xiu.chau@gmail.com>
2017-03-22 10:34:45 -0700

    Prep for cloudinary

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -32,9 +32,9 @@ router.post('/', (req, res, next) => {
   //   api_secret: 'a676b67565c6767a6767d6767f676fe1'
   // });
 
-  cloudinary.uploader.upload(cover_photo, (result) => {
+  cloudinary.v2.uploader.upload(cover_photo, (result) => {
     console.log(result)
-  }, { resource_type: "raw" });
+  }, { resource_type: "auto" });
 
   // knex('trips')
   //   .insert({
