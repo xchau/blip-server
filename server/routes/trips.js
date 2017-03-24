2deb37d507eebce546ba8e0fd08a2f35b7614feb
Minh Chau <xiu.chau@gmail.com>
2017-03-24 12:06:58 -0700

    Require jwt

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -91,8 +91,8 @@ router.patch('/publish', authorize, (req, res, next) => {
   console.log('authorized');
   console.log(req.claim);
 
-  knex('trips')
-    .
+  // knex('trips')
+  //   .
 });
 
 module.exports = router;
