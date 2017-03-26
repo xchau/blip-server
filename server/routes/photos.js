21eb67e9bbf1ecb6e0030d5f37366445b6e818f3
Minh Chau <xiu.chau@gmail.com>
2017-03-26 13:33:29 -0700

    Retrieve photos by entry id

--- server/routes/photos.js
+++ server/routes/photos.js
@@ -4,10 +4,10 @@ const knex = require('../../knex');
 const router = require('express').Router();
 
 router.get('/:id', (req, res, next) => {
-  const tripId = req.params.id;
+  const entryId = req.params.id;
 
   knex('photos')
-    .where('trip_id', tripId)
+    .where('entry_id', entryId)
     .then((photos) => {
       if (!photos.length) {
         throw boom.create(400, 'No images available');
