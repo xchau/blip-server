96b8a436ea956dbacf2bb5108c825010e9eb6de1
Minh Chau <xiu.chau@gmail.com>
2017-03-26 12:57:04 -0700

    Test /trips/entries

--- server/routes/entries.js
+++ server/routes/entries.js
@@ -2,12 +2,10 @@
 
 const { camelizeKeys, decamelizeKeys } = require('humps');
 const boom = require('boom');
+const cloudinary = require('../../cloudinary');
 const jwt = require('jsonwebtoken');
 const knex = require('../../knex');
-const multer = require('multer');
 const router = require('express').Router();
-
-const upload = multer();
 
 const authorize = function(req, res, next) {
   const userToken = req.get('Authorization').split(' ')[1];
@@ -42,9 +40,57 @@ router.get('/:id/entries', (req, res, next) => {
 });
 
 router.post('/entries', authorize, (req, res, next) => {
-  console.log(req.body);
-  const { entry_title, note } = decamelizeKeys(req.body);
+  const { trip_id,
+    entry_title,
+    note,
+    caption,
+    image
+  } = decamelizeKeys(req.body);
 
+  const photo = `data:image/jpg;base64,${image}`;
+
+  let insertedEntry;
+  let photoObj;
+
+  cloudinary.v2.uploader.upload(photo, {
+    quality: 50,
+  }, (error, result) => {
+    if (error) {
+      console.error(`Cloudinary Error: ${error}`);
+    }
+
+    photoObj = result;
+
+    knex('entries')
+      .insert({
+        entry_title,
+        note,
+        trip
+      }, '*')
+      .then((entries) => {
+        insertedEntry = camelizeKeys(entries[0]);
+
+        return knex('photos')
+          .insert({
+            entry_id: insertedEntry.id,
+            caption: insertedEntry.caption,
+            photo_url: photoObj.secure_url,
+          }, '*');
+      })
+      .then((photos) => {
+        const photo = camelizeKeys(photos[0]);
+
+        const resObject = {
+          photo,
+          insertedTrip
+        };
+
+        res.send(resObject);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  });
 });
 
 module.exports = router;
